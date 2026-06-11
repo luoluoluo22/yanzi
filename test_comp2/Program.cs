acb534b2aa95b47fb510b11b59c0951eefc5bdd2
luoluoluo22 <1137583371@qq.com>
2026-06-11 16:25:20 +0800

    chore: clean up test files, temporary scripts, and design reference documents

--- test_comp2/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Reflection;
-using Microsoft.Web.WebView2.Core;
-
-var type = typeof(CoreWebView2Cookie);
-var prop = type.GetProperty("Expires");
-Console.WriteLine(prop.PropertyType.FullName);
