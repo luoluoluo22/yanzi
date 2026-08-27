97d5d47b83e64096ebfcac828f144fd327f73591
luoluoluo22 <1137583371@qq.com>
2026-08-27 14:03:49 +0800

    fix(mac): 全面对齐Windows全局配色方案与动态主题，修复浅色模式下字体发白不可见问题

--- src/Yanzi.Avalonia/Converters.cs
+++ src/Yanzi.Avalonia/Converters.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Globalization;
+using Avalonia;
 using Avalonia.Data.Converters;
 using Avalonia.Media;
+using Avalonia.Styling;
 
 namespace Yanzi.Avalonia;
 
@@ -14,5 +16,17 @@ public static class Converters
         new FuncValueConverter<bool?, IBrush>(b => b == true ? new SolidColorBrush(Color.Parse("#FF3B82F6")) : new SolidColorBrush(Color.Parse("#FFAAAAAA")));
 
     public static readonly IValueConverter EmptyTextBrushConverter =
-        new FuncValueConverter<bool?, IBrush>(b => b == true ? new SolidColorBrush(Color.Parse("#66FFFFFF")) : new SolidColorBrush(Color.Parse("#E0FFFFFF")));
+        new FuncValueConverter<bool?, IBrush>(b =>
+        {
+            var app = Application.Current;
+            if (app != null)
+            {
+                var theme = app.ActualThemeVariant;
+                if (b == true && app.TryGetResource("BrushTextMuted", theme, out var mutedObj) && mutedObj is IBrush mutedBrush)
+                    return mutedBrush;
+                if (b == false && app.TryGetResource("BrushTextMain", theme, out var mainObj) && mainObj is IBrush mainBrush)
+                    return mainBrush;
+            }
+            return b == true ? new SolidColorBrush(Color.Parse("#888888")) : new SolidColorBrush(Color.Parse("#333333"));
+        });
 }
